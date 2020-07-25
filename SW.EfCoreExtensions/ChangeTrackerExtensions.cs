7d5f089ce266d4b954452fa0c8acda3802747d95
Samer Awajan <samer@simplify9.com>
2020-07-25 13:20:07 +0300

    using reflection for setting common properties

--- SW.EfCoreExtensions/ChangeTrackerExtensions.cs
+++ SW.EfCoreExtensions/ChangeTrackerExtensions.cs
@@ -4,6 +4,7 @@ using SW.PrimitiveTypes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +17,8 @@ namespace SW.EfCoreExtensions
             changeTracker.DetectChanges();
 
             var timestamp = DateTime.UtcNow;
+            var nameIdentifier = requestContext?.GetNameIdentifier();
+            var tenantId = requestContext?.GetTenantId();
 
             foreach (var entry in changeTracker.Entries())
             {
@@ -23,37 +26,35 @@ namespace SW.EfCoreExtensions
                 if (entry.Entity is ISoftDelete && entry.State == EntityState.Deleted)
                 {
                     entry.State = EntityState.Modified;
-                    entry.Property(nameof(ISoftDelete.Deleted)).CurrentValue = true;
+                    TrySetProperty(entry.Entity, nameof(ISoftDelete.Deleted), true);
 
                     if (entry.Entity is IHasDeletionTime)
-
-                        entry.Property(nameof(IHasDeletionTime.DeletedOn)).CurrentValue = timestamp;
+                        TrySetProperty(entry.Entity, nameof(IHasDeletionTime.DeletedOn), timestamp);
 
                     if (entry.Entity is IDeletionAudited)
-
-                        entry.Property(nameof(IDeletionAudited.DeletedBy)).CurrentValue = requestContext?.GetNameIdentifier();
-
+                        TrySetProperty(entry.Entity, nameof(IDeletionAudited.DeletedBy), nameIdentifier);
                 }
 
                 if (entry.Entity is IHasCreationTime && entry.State == EntityState.Added)
-
-                    entry.Property(nameof(IHasCreationTime.CreatedOn)).CurrentValue = timestamp;
+                    TrySetProperty(entry.Entity, nameof(IHasCreationTime.CreatedOn), timestamp);
 
                 if (entry.Entity is ICreationAudited && entry.State == EntityState.Added)
-
-                    entry.Property(nameof(ICreationAudited.CreatedBy)).CurrentValue = requestContext?.GetNameIdentifier();
+                    TrySetProperty(entry.Entity, nameof(ICreationAudited.CreatedBy), nameIdentifier);
 
                 if (entry.Entity is IHasModificationTime && (entry.State == EntityState.Added || entry.State == EntityState.Modified))
-
-                    entry.Property(nameof(IHasModificationTime.ModifiedOn)).CurrentValue = timestamp;
+                    TrySetProperty(entry.Entity, nameof(IHasModificationTime.ModifiedOn), timestamp);
 
                 if (entry.Entity is IModificationAudited && (entry.State == EntityState.Added || entry.State == EntityState.Modified))
-
-                    entry.Property(nameof(IModificationAudited.ModifiedBy)).CurrentValue = requestContext?.GetNameIdentifier();
-
+                    TrySetProperty(entry.Entity, nameof(IModificationAudited.ModifiedBy), nameIdentifier);
             }
         }
 
+        private static void TrySetProperty(object entity, string propertyName, object value)
+        {
+            var prop = entity.GetType().GetProperty(propertyName);
+            var setMethod = prop.GetSetMethod() ?? prop.GetSetMethod(true);
+            if (setMethod != null) setMethod.Invoke(entity, new object[] { value });
+        }
 
         async public static Task DispatchDomainEvents(this ChangeTracker changeTracker, IDomainEventDispatcher domainEventDispatcher)
         {
