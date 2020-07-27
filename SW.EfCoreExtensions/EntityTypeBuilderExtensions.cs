fd3896babad10bdbdc9497753b560fdca1411762
Samer Awajan <samer@simplify9.com>
2020-07-27 16:29:06 +0300

    refactoring

--- SW.EfCoreExtensions/EntityTypeBuilderExtensions.cs
+++ SW.EfCoreExtensions/EntityTypeBuilderExtensions.cs
@@ -1,56 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using SW.EfCoreExtensions;
 using SW.PrimitiveTypes;
-using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Text;
 
 namespace SW.EfCoreExtensions
 {
     public static class EntityTypeBuilderExtensions
     {
-        public static EntityTypeBuilder<T> HasQueryFilter<T>(this EntityTypeBuilder<T> builder, QueryFilter queryFilter, RequestContext requestContext = null) where T : class
-        {
-            switch (queryFilter)
-            {
-                case QueryFilter.SoftDeletion:
-                    if (typeof(ISoftDelete).IsAssignableFrom(typeof(T)))
-                        return builder.HasQueryFilter(item => !EF.Property<bool>(item, nameof(ISoftDelete.Deleted)));
-                    
-                    break;
+        public static EntityTypeBuilder<T> HasQueryFilterForSoftDeletion<T>(this EntityTypeBuilder<T> builder) where T : class, ISoftDelete
+            => builder.HasQueryFilter(item => !EF.Property<bool>(item, nameof(ISoftDelete.Deleted)));
 
-                case QueryFilter.Tenancy:
-                    if (typeof(IHasTenant).IsAssignableFrom(typeof(T)))
-                        return builder.HasQueryFilter(item => EF.Property<int>(item, nameof(IHasTenant.TenantId)) == requestContext.GetTenantId().Value);
-                    
-                    else if (typeof(IHasOptionalTenant).IsAssignableFrom(typeof(T)))
-                        return builder.HasQueryFilter(item => EF.Property<int?>(item, nameof(IHasOptionalTenant.TenantId)) == requestContext.GetTenantId() || EF.Property<int?>(item, nameof(IHasOptionalTenant.TenantId)) == null);
-                    
-                    break;
+        public static EntityTypeBuilder<T> HasQueryFilterForTenancy<T>(this EntityTypeBuilder<T> builder, RequestContext requestContext) where T : class, IHasTenant
+            => builder.HasQueryFilter(item => EF.Property<int>(item, nameof(IHasTenant.TenantId)) == requestContext.GetTenantId().Value);
 
-                case QueryFilter.SoftDeletion | QueryFilter.Tenancy:
-                    if (typeof(IHasTenant).IsAssignableFrom(typeof(T)) && typeof(ISoftDelete).IsAssignableFrom(typeof(T)))
-                        return builder.HasQueryFilter(item => !EF.Property<bool>(item, nameof(ISoftDelete.Deleted)) && EF.Property<int>(item, nameof(IHasTenant.TenantId)) == requestContext.GetTenantId().Value);
+        public static EntityTypeBuilder<T> HasQueryFilterForOptionalTenancy<T>(this EntityTypeBuilder<T> builder, RequestContext requestContext) where T : class, IHasOptionalTenant
+            => builder.HasQueryFilter(item => EF.Property<int?>(item, nameof(IHasOptionalTenant.TenantId)) == requestContext.GetTenantId() || EF.Property<int?>(item, nameof(IHasOptionalTenant.TenantId)) == null);
 
-                    else if (typeof(IHasOptionalTenant).IsAssignableFrom(typeof(T)) && typeof(ISoftDelete).IsAssignableFrom(typeof(T)))
-                        return builder.HasQueryFilter(item => !EF.Property<bool>(item, nameof(ISoftDelete.Deleted)) && (EF.Property<int?>(item, nameof(IHasOptionalTenant.TenantId)) == requestContext.GetTenantId() || EF.Property<int?>(item, nameof(IHasOptionalTenant.TenantId)) == null));
-                    
-                    //else if (typeof(IHasTenant).IsAssignableFrom(typeof(T)))
-                    //    return builder.HasQueryFilter(item => !EF.Property<bool>(item, nameof(ISoftDelete.Deleted)));
-                    
-                    //else if (typeof(IHasOptionalTenant).IsAssignableFrom(typeof(T)))
-                    //    return builder.HasQueryFilter(item => !EF.Property<bool>(item, nameof(ISoftDelete.Deleted)));
-                    
-                    //else if (typeof(ISoftDelete).IsAssignableFrom(typeof(T)))
-                    //    return builder.HasQueryFilter(item => !EF.Property<bool>(item, nameof(ISoftDelete.Deleted)));
-                    
-                    break;
+        public static EntityTypeBuilder<T> HasQueryFilterForSoftDeletionAndTenancy<T>(this EntityTypeBuilder<T> builder, RequestContext requestContext) where T : class, ISoftDelete, IHasTenant
+            => builder.HasQueryFilter(item => !EF.Property<bool>(item, nameof(ISoftDelete.Deleted)) && EF.Property<int>(item, nameof(IHasTenant.TenantId)) == requestContext.GetTenantId().Value);
 
-            }
-
-            return builder;        
-        }
+        public static EntityTypeBuilder<T> HasQueryFilterForSoftDeletionAndOptionalTenancy<T>(this EntityTypeBuilder<T> builder, RequestContext requestContext) where T : class, ISoftDelete, IHasOptionalTenant
+            => builder.HasQueryFilter(item => !EF.Property<bool>(item, nameof(ISoftDelete.Deleted)) && (EF.Property<int?>(item, nameof(IHasOptionalTenant.TenantId)) == requestContext.GetTenantId() || EF.Property<int?>(item, nameof(IHasOptionalTenant.TenantId)) == null));
     }
 }
