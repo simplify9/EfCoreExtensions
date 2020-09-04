b69931f684a425f197e56633d39b320da4e2a470
Muhannad Al-Khatib <muhannad@simplify9.com>
2020-09-04 15:34:37 +0300

    Added unit tests for string and integer array seperator delimited

---
+++ SW.EfCoreExtensions.UnitTests/Domain/SomeData.cs
@@ -0,0 +1,9 @@
+namespace SW.EfCoreExtensions.UnitTests.Domain
+{
+    class SomeData
+    {
+        public int Id { get; set; }
+        public string[] StringArray { get; set; }
+        public int[] IntegerArray { get; set; }
+    }
+}
--- SW.EfCoreExtensions.UnitTests/EfCoreTests.cs
+++ SW.EfCoreExtensions.UnitTests/EfCoreTests.cs
@@ -7,8 +7,11 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SW.EfCoreExtensions.UnitTests.Domain;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bogus.DataSets;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace SW.EfCoreExtensions.UnitTests
 {
@@ -51,7 +54,87 @@ namespace SW.EfCoreExtensions.UnitTests
 
             Assert.AreEqual(1, countTickets);
         }
+        [TestMethod]
+        async public Task TestIsSeparatorDelimitedString()
+        {
+            using var scope = server.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<TestDbContext>();
+            var first = await dbContext.Set<SomeData>().FirstOrDefaultAsync();
+            var expected = new[]{ "a","b","c"};
+            Assert.IsTrue(expected.SequenceEqual(first.StringArray));
+            await using var command = dbContext.Database.GetDbConnection().CreateCommand();
+            command.CommandText = $"SELECT * From {nameof(SomeData)}";
+            await dbContext.Database.OpenConnectionAsync();
+            await using var result = await command.ExecuteReaderAsync();
+            await result.ReadAsync();
+            Assert.AreEqual("a,b,c", result.GetString(1));
+            var readByWhereEquals = await dbContext.Set<SomeData>().FirstOrDefaultAsync(
+                x => x.StringArray == new[] {"d", "e", "f"});
+            Assert.IsNotNull(readByWhereEquals);
+            Assert.IsTrue(readByWhereEquals.StringArray.Contains("e"));
+            var toInsert = new SomeData {StringArray = new[] {"g", "h", "i"}};
+            dbContext.Add(toInsert);
+            await dbContext.SaveChangesAsync();
+            var inserted = await dbContext.Set<SomeData>()
+                .OrderByDescending(x => x.Id).FirstOrDefaultAsync();
+            Assert.IsTrue(toInsert.StringArray.SequenceEqual(inserted.StringArray));
+            
+            var toInsertNull = new SomeData();
+            dbContext.Add(toInsertNull);
+            await dbContext.SaveChangesAsync();
+            var insertedNull = await dbContext.Set<SomeData>()
+                .OrderByDescending(x => x.Id).FirstOrDefaultAsync();
+            Assert.IsNull(insertedNull.StringArray);
+            
+            var commandInsertManually = $"INSERT INTO SomeData ({nameof(inserted.StringArray)}) VALUES ('j,k,l')";
+            await dbContext.Database.ExecuteSqlRawAsync(commandInsertManually);
+            var insertedManually = await dbContext.Set<SomeData>()
+                .OrderByDescending(x => x.Id).FirstOrDefaultAsync();
+            Assert.IsTrue(insertedManually.StringArray.SequenceEqual(new []{"j","k","l"}));
 
+        }
+        
+        [TestMethod]
+        async public Task TestIsSeparatorDelimitedInteger()
+        {
+            using var scope = server.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<TestDbContext>();
+            var first = await dbContext.Set<SomeData>().FirstOrDefaultAsync();
+            var expected = new[]{ 1,2,3 };
+            Assert.IsTrue(expected.SequenceEqual(first.IntegerArray));
+            await using var command = dbContext.Database.GetDbConnection().CreateCommand();
+            command.CommandText = $"SELECT * From {nameof(SomeData)}";
+            await dbContext.Database.OpenConnectionAsync();
+            await using var result = await command.ExecuteReaderAsync();
+            await result.ReadAsync();
+            Assert.AreEqual("1,2,3", result.GetString(2));
+            var readByWhereEquals = await dbContext.Set<SomeData>().FirstOrDefaultAsync(
+                x => x.IntegerArray == new[] {4, 5, 6});
+            Assert.IsNotNull(readByWhereEquals);
+            Assert.IsTrue(readByWhereEquals.IntegerArray.Contains(4));
+            
+            var toInsert = new SomeData {IntegerArray = new[] {7,8,9}};
+            dbContext.Add(toInsert);
+            await dbContext.SaveChangesAsync();
+            var inserted = await dbContext.Set<SomeData>()
+                .OrderByDescending(x => x.Id).FirstOrDefaultAsync();
+            Assert.IsTrue(toInsert.IntegerArray.SequenceEqual(inserted.IntegerArray));
+            
+            var toInsertNull = new SomeData();
+            dbContext.Add(toInsertNull);
+            await dbContext.SaveChangesAsync();
+            var insertedNull = await dbContext.Set<SomeData>()
+                .OrderByDescending(x => x.Id).FirstOrDefaultAsync();
+            Assert.IsNull(insertedNull.IntegerArray);
+            
+            var commandInsertManually = $"INSERT INTO SomeData ({nameof(inserted.IntegerArray)}) VALUES ('7,8,9')";
+            await dbContext.Database.ExecuteSqlRawAsync(commandInsertManually);
+            var insertedManually = await dbContext.Set<SomeData>()
+                .OrderByDescending(x => x.Id).FirstOrDefaultAsync();
+            Assert.IsTrue(insertedManually.IntegerArray.SequenceEqual(new[]{ 7,8,9 }));
+
+            
+        }
         [TestMethod]
         async public Task TestSoftDeletionAndOptionalTenantFilters()
         {
