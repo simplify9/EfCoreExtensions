53e930bf65489208d14db620c1754a887b81e8b7
Samer Awajan <samer@simplify9.com>
2020-12-29 12:51:25 +0200

    more extensions

---
+++ SW.EfCoreExtensions.PgSql/IHostExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace SW.EfCoreExtensions
+{
+
+    public static class IHostExtensions
+    {
+        public static IHost MigrateDatabase<TDbContext>(this IHost host) where TDbContext : DbContext
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                using (var appContext = scope.ServiceProvider.GetRequiredService<TDbContext>())
+                {
+                    try
+                    {
+                        appContext.Database.Migrate();
+                    }
+                    catch (Exception ex)
+                    {
+                        //Log errors or do anything you think it's needed
+                        throw;
+                    }
+                }
+            }
+
+            return host;
+        }
+    }
+}
