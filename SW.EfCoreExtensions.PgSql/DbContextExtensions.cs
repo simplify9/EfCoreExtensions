d9ea17947d820143a0067adf48d38d38b92cf940
Samer Awajan <samer@simplify9.com>
2020-12-29 13:36:06 +0200

    more extensions

---
+++ SW.EfCoreExtensions.PgSql/DbContextExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SW.PrimitiveTypes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SW.EfCoreExtensions
+{
+    public static class DbContextExtensions
+    {
+        async public static Task DeleteByKeyAsync<TEntity>(this DbContext dbContext, object key) where TEntity : class
+        {
+            var entity = await dbContext.FindAsync<TEntity>(key);
+            dbContext.Remove(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public static async Task<List<TEntity>> ListAsync<TEntity>(this DbContext dbContext, ISpecification<TEntity> spec = null) where TEntity : class
+        {
+            var query = dbContext.Set<TEntity>().AsQueryable();
+            if (spec != null)
+                query = query.Where(spec.Criteria);
+            return await query.ToListAsync();
+        }
+
+        public static List<TEntity> List<TEntity>(this DbContext dbContext, ISpecification<TEntity> spec = null) where TEntity : class
+        {
+            var query = dbContext.Set<TEntity>().AsQueryable();
+            if (spec != null)
+                query = query.Where(spec.Criteria);
+            return query.ToList();
+        }
+    }
+}
---
+++ SW.EfCoreExtensions.PgSql/OwnedNavigationBuilderExtensions.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SW.PrimitiveTypes;
+
+
+namespace SW.EfCoreExtensions
+{
+    public static class OwnedNavigationBuilderExtensions
+    {
+
+        public static OwnedNavigationBuilder<TOwner, StreetAddress> BuildStreetAddress<TOwner>(this OwnedNavigationBuilder<TOwner, StreetAddress> builder, string prefix = null, bool required = false)
+            where TOwner : class
+        {
+            //builder.Property(p => p.Street).IsSeparatorDelimited();
+            builder.Property(p => p.Country).HasColumnName($"{prefix}country").IsCode(2, required: required);
+            builder.Property(p => p.City).HasColumnName($"{prefix}city").HasMaxLength(100).IsRequired(required);
+            builder.Property(p => p.State).HasColumnName($"{prefix}state").HasMaxLength(100);
+            builder.Property(p => p.PostCode).HasColumnName($"{prefix}post_code").HasMaxLength(20).IsUnicode(false);
+            builder.Property(p => p.Street).HasColumnName($"{prefix}street").HasMaxLength(1024);
+
+            return builder;
+        }
+
+        public static OwnedNavigationBuilder<TOwner, GeoPosition> BuildGeoPosition<TOwner>(this OwnedNavigationBuilder<TOwner, GeoPosition> builder, string prefix = null)
+            where TOwner : class
+        {
+            builder.Property(p => p.Latitude).HasColumnName($"{prefix}latitude").HasColumnType("decimal(11,8)");
+            builder.Property(p => p.Longitude).HasColumnName($"{prefix}longitude").HasColumnType("decimal(11,8)");
+
+            return builder;
+        }
+
+        public static OwnedNavigationBuilder<TOwner, Contact> BuildContact<TOwner>(this OwnedNavigationBuilder<TOwner, Contact> builder, string prefix = null, bool required = false)
+            where TOwner : class
+        {
+            //builder.Property(p => p.Phones).IsSeparatorDelimited();
+            //builder.Property(p => p.Emails).IsSeparatorDelimited();
+            builder.Property(p => p.Name).HasColumnName($"{prefix}name").HasMaxLength(100).IsRequired(required);
+            builder.Property(p => p.Phones).HasColumnName($"{prefix}phones").HasMaxLength(100);
+            builder.Property(p => p.Emails).HasColumnName($"{prefix}emails").HasMaxLength(1024);
+
+            return builder;
+        }
+
+        public static OwnedNavigationBuilder<TOwner, Dimensions> BuildDimensions<TOwner>(this OwnedNavigationBuilder<TOwner, Dimensions> builder, string prefix = null, bool required = false)
+          where TOwner : class
+        {
+            builder.Property(d => d.Height).HasColumnType("decimal(16,6)").HasColumnName($"{prefix}height");
+            builder.Property(d => d.Length).HasColumnType("decimal(16,6)").HasColumnName($"{prefix}length");
+            builder.Property(d => d.Width).HasColumnType("decimal(16,6)").HasColumnName($"{prefix}width");
+            builder.Property(d => d.Unit).IsUnicode(false).HasMaxLength(2).HasColumnName($"{prefix}dimension_unit");
+            builder.Property(d => d.Unit).HasConversion<string>();
+
+            return builder;
+        }
+
+        public static OwnedNavigationBuilder<TOwner, Weight> BuildWeight<TOwner>(this OwnedNavigationBuilder<TOwner, Weight> builder, string prefix = null, bool required = false)
+          where TOwner : class
+        {
+            builder.Property(d => d.Value).HasColumnType("decimal(16,6)").HasColumnName($"{prefix}weight");
+            builder.Property(d => d.Unit).IsUnicode(false).HasMaxLength(2).IsFixedLength().HasColumnName($"{prefix}weight_unit");
+            builder.Property(d => d.Unit).HasConversion<string>();
+
+            return builder;
+        }
+
+        public static OwnedNavigationBuilder<TOwner, Money> BuildMoney<TOwner>(this OwnedNavigationBuilder<TOwner, Money> builder, string prefix = null, bool required = false)
+         where TOwner : class
+        {
+            builder.Property(e => e.Amount).HasColumnType("decimal(16,6)").HasColumnName($"{prefix}value");
+            builder.Property(e => e.Currency).IsCode(3, false).HasColumnName($"{prefix}currency");
+
+            return builder;
+        }
+
+        public static OwnedNavigationBuilder<TOwner, Audit> BuildAudit<TOwner>(this OwnedNavigationBuilder<TOwner, Audit> builder)
+            where TOwner : class
+        {
+            builder.Property(d => d.CreatedBy).IsUnicode(false).HasMaxLength(50).HasColumnName("created_by");
+            builder.Property(d => d.ModifiedBy).IsUnicode(false).HasMaxLength(50).HasColumnName("modified_by");
+            builder.Property(d => d.CreatedOn).HasColumnName("created_on");
+            builder.Property(d => d.ModifiedOn).HasColumnName("modified_on");
+
+            return builder;
+        }
+
+    }
+}
